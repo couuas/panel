f965a0825eec1932d486796de027a51be511a9e7
couuas <couuas@gmail.com>
2025-03-25 13:35:36 +0800

    Create hy2ip.sh

---
+++ hy2ip.sh
@@ -0,0 +1,81 @@
+#!/bin/bash
+# 字体上色
+A() {
+    echo -e "\033[32m$1\033[0m"
+}
+B() {
+    echo -e "\033[31m$1\033[0m"
+}
+C() {
+    C() {
+    local D=$(hostname)
+    local E=$(echo "$D" | awk -F'[s.]' '{print $2}')
+    local F="s${E}.serv00.us.kg"
+    local ip=$(dig +short "$F")
+    if [[ -n "$ip" ]]; then
+        echo "$ip"
+    else
+        echo ""
+    fi
+}
+    return 1  
+}
+K() {
+    local L="$1"
+    local M="$2"
+    if [[ ! -f "$L" ]]; then
+        B "配置文件 $L 不存在！"
+        return 1
+    fi
+    jq --arg N "$M" '
+        (.inbounds[] | select(.tag == "hysteria-in") | .listen) = $N
+    ' "$L" > temp.json && mv temp.json "$L"
+
+    if [[ $? -eq 0 ]]; then
+        A "SingBox 配置文件成功更新IP为 $M"
+    else
+        B "更新配置文件失败！"
+        return 1
+    fi
+}
+O() {
+    local P="$1"
+    local Q="$2"
+    if [[ ! -f "$P" ]]; then
+        B "配置文件 $P 不存在！"
+        return 1
+    fi
+    jq --arg R "$Q" '
+        .HY2IP = $R
+    ' "$P" > temp.json && mv temp.json "$P"
+
+    if [[ $? -eq 0 ]]; then
+        A "Config 配置文件成功更新IP为 $Q"
+    else
+        B "更新配置文件失败！"
+        return 1
+    fi
+}
+S() {
+    local T="$HOME/serv00-play/singbox/config.json"
+    local U="$HOME/serv00-play/singbox/singbox.json"
+    local V=$(C)
+    echo "有效 IP: $V"
+    if [[ -z "$V" ]]; then
+        B "没有可用 IP！"
+        return 1  
+    fi
+    K "$T" "$V"
+    O "$U" "$V"
+    echo "正在重启 sing-box..."
+    W
+    sleep 5
+    X
+}
+W() {
+    cd ~/serv00-play/singbox/ && bash killsing-box.sh
+}
+X() {
+    cd ~/serv00-play/singbox/ && bash start.sh
+}
+S
